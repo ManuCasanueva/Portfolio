b5e6d12cb925aa627995ed8e2b9ce67a174cd600
ManuCasanueva <manucasanueva3@gmail.com>
2023-05-29 14:00:13 -0300

    Resume

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,8 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import cv from "../assets/Curriculum-Manuel.pdf"
-
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -21,19 +19,12 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [text])
+  }, [text]);
 
   function downloadCV() {
-    const cvContent = cv;
-    const cvBlob = new Blob([cvContent], { type: 'application/pdf' });
-    const cvURL = URL.createObjectURL(cvBlob);
-    const downloadLink = document.createElement('a');
-    downloadLink.href = cvURL;
-    downloadLink.download = "Curriculum-Manuel.pdf";
-    downloadLink.click();
-    URL.revokeObjectURL(cvURL);
+    const cvURL = "https://drive.google.com/file/d/1Yrh5SYIlkqO2IP8pzf7jp0jsC_Idxu9G/view";
+    window.open(cvURL, "_blank");
   }
-  
 
   const tick = () => {
     let i = loopNum % toRotate.length;
@@ -72,11 +63,12 @@ export const Banner = () => {
                 <h1>{`Hi! I'm Manuel Casanueva`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Full Stack Developer graduated from "Henry" a very demanding bootcamp, which has more than +800 hours, in the 4 months that last, I learned technologies like React-Redux, javascript, PostgreSQL, Express y Next.Js in the Back End.
                     Experience as Henry Assistant, which is based on management of a group of students, fostering their Soft Skills learning</p>
-                    <span className="navbar-text" style={{ marginTop:5 }} >
-
-                      <button id="download-button" onClick={downloadCV} className="vvd"   class="btn-descargar">Download CV</button>  
+                    <span className="navbar-text" style={{ marginTop:5 }}>
+                      <button id="download-button" onClick={downloadCV}  className="btn-descargar">View Resume</button>  
                     </span>
               </div>}
+           
+
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
